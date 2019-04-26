f266a9f93bd05b8c90cbed48c2f578d7169174f9
wangbiao <wangbiao@ybm100.com>
2019-04-26 11:31:57 +0800

    接入apollo测试

--- src/main/java/com/bill/test/Application.java
+++ src/main/java/com/bill/test/Application.java
@@ -1,11 +1,12 @@
 package com.bill.test;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableApolloConfig
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}

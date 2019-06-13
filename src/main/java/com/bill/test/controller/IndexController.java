de33eab200c533dbc81066da0bad8abb2708ab52
wangbiao <wangbiao@ybm100.com>
2019-06-13 15:34:56 +0800

    提交RestTemplateController

--- src/main/java/com/bill/test/controller/IndexController.java
+++ src/main/java/com/bill/test/controller/IndexController.java
@@ -3,11 +3,9 @@ package com.bill.test.controller;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.UUID;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.bill.test.bean.User;
 
@@ -27,11 +25,12 @@ public class IndexController {
 
 	// @RequestParam 简单类型的绑定，可以出来get和post
 	@RequestMapping(value = "/get")
-	public HashMap<String, Object> get(@RequestParam String name) {
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		map.put("title", "hello world");
-		map.put("name", name);
-		return map;
+	public User get(@RequestParam(value="name") String name) {
+		User user=new User();
+		user.setDate(new Date());
+		user.setName(name);
+		user.setId(UUID.randomUUID().version());
+		return user;
 	}
 
 	// @PathVariable 获得请求url中的动态参数
---
+++ src/main/java/com/bill/test/controller/RestTemplateController.java
@@ -0,0 +1,46 @@
+package com.bill.test.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.bill.test.bean.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : wangbiao
+ * @version V1.0
+ * @Project: spring-boot-demo-bill
+ * @Package com.bill.test.controller
+ * @Description: TODO Springboot — 用更优雅的方式发HTTP请求(RestTemplate详解)
+ * @date Date : 2019年06月13日 14:11
+ */
+@RestController
+@Slf4j
+public class RestTemplateController {
+
+    private RestTemplate restTemplate=new RestTemplate();
+
+    @RequestMapping("/getForObject")
+    public User getForObject(){
+        String url="http://localhost:8088/index/get?name={name}";
+        Map map=new HashMap<String,String>();
+        map.put("name","http");
+        log.info("请求url:"+url+"，请求参数："+ JSONObject.toJSONString(map));
+        User result = restTemplate.getForObject(url, User.class, map);
+        log.info("使用RestTemplate发送Http请求，获取的结果："+JSONObject.toJSONString(result));
+        return result;
+    }
+
+    @RequestMapping("/getUser")
+    public User getUser(){
+        String url="http://localhost:8088/index/get/{1}/{2}";
+        log.info("请求url:"+url);
+        User result = restTemplate.getForObject(url, User.class, 2,"bill");
+        log.info("使用RestTemplate发送Http请求，获取的结果："+JSONObject.toJSONString(result));
+        return result;
+    }
+}

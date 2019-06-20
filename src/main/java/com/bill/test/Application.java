6742164cc3616d7ca0986871c381d352b45df525
wangbiao <wangbiao@ybm100.com>
2019-06-20 10:06:38 +0800

    使用alibaba的代码扫描插件P3C进行扫描，根据提示做修改

--- src/main/java/com/bill/test/Application.java
+++ src/main/java/com/bill/test/Application.java
@@ -4,9 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+/**
+ * @EnableApolloConfig
+ */
 @SpringBootApplication
 @EnableAsync
-//@EnableApolloConfig
+/**
+ * @author: wangbiao
+ * @version 1.0.0
+ * @Project spring-boot-demo-bill
+ */
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
